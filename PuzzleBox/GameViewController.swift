97b4f020c86f98381e0ba07ec2f122afd7495f59
Invader Zim <pwdelete@gmail.com>
2022-05-04 17:27:31 -0700

    Added initial setup functions.

--- PuzzleBox/GameViewController.swift
+++ PuzzleBox/GameViewController.swift
@@ -15,8 +15,46 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+    private var scnView: SCNView!
+    private var scnScene: SCNScene!
+    private var cameraNode: SCNNode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupScene()
+        setupCamera()
+    }
+    
+    // MARK: - Setup functions
+    
+    private func setupView() {
+        scnView = self.view as? SCNView
+        scnView.allowsCameraControl = true  // allow standard camera controls with swiping
+        scnView.showsStatistics = true
+        scnView.autoenablesDefaultLighting = true
+        scnView.isPlaying = true  // prevent SceneKit from entering a "paused" state, if there isn't anything to animate
+    }
+    
+    private func setupScene() {
+        scnScene = SCNScene()
+        scnScene.background.contents = "Background_Diffuse.png"
+        scnView.scene = scnScene
+    }
+    
+    private func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        rotateCameraAroundBoardCenter(deltaAngle: -.pi/4)  // move up 45 deg
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    // rotate camera around board x-axis, while continuing to point at board center
+    private func rotateCameraAroundBoardCenter(deltaAngle: CGFloat) {
+        cameraNode.transform = SCNMatrix4Rotate(cameraNode.transform, Float(deltaAngle), 1, 0, 0)
+        let cameraAngle = CGFloat(cameraNode.eulerAngles.x)
+        let cameraDistance = max(9.7 * scnView.frame.height / scnView.frame.width, 15)
+        cameraNode.position = SCNVector3(0, -cameraDistance * sin(cameraAngle), cameraDistance * cos(cameraAngle))
     }
 }
