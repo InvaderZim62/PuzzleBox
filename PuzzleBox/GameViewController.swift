aceda178e8871ab3b7a7e9a5e2e6c27a5f5039f8
Invader Zim <pwdelete@gmail.com>
2022-05-05 13:59:29 -0700

    Added tap and pan gestures to the scene.  Toggle highlighting of side node when tapped.  Disable camera panning when side node is highlighted/selected.

--- PuzzleBox/GameViewController.swift
+++ PuzzleBox/GameViewController.swift
@@ -23,11 +23,25 @@ struct Box {
     static let wallThickness = 0.5
 }
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UIGestureRecognizerDelegate {  // delegate needed for func gestureRecognizer (bottom of file)
     
-    private var scnView: SCNView!
-    private var scnScene: SCNScene!
-    private var cameraNode: SCNNode!
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    
+    var isCameraPanning = true
+    var sideNodes = [MovableSideNode]()
+    var selectedSideNode: MovableSideNode? {
+        didSet {
+            sideNodes.forEach { $0.resetColor() }  // reset all colors, before highlighting selected node (if any)
+            if let selectedNode = selectedSideNode {
+                selectedNode.highlightColor()
+                isCameraPanning = false  // don't allow camera panning when a side node is selected
+            } else {
+                isCameraPanning = true
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +49,22 @@ class GameViewController: UIViewController {
         setupScene()
         setupCamera()
         createPuzzleBox()
+        
+        // add gestures to scnView
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        pan.maximumNumberOfTouches = 1  // prevents panning during rotation
+        pan.delegate = self  // allows system to call gestureRecognizer (bottom of file)
+        scnView.addGestureRecognizer(pan)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tap.require(toFail: pan)  // prevents tap from being called right before pan gesture ends (deselecting side node)
+        scnView.addGestureRecognizer(tap)
+        
+        // require my pan gesture to fail, before allowing camera's pan gesture to work (force my pan to fail in handlePan, if isCameraPanning)
+        let panGestures = scnView.gestureRecognizers!.filter { $0 is UIPanGestureRecognizer } as! [UIPanGestureRecognizer]  // my pan and default camera pan
+        if !panGestures.isEmpty {
+            let cameraPanGesture = panGestures.first!
+            cameraPanGesture.require(toFail: pan)
+        }
     }
     
     private func createPuzzleBox() {
@@ -44,23 +74,60 @@ class GameViewController: UIViewController {
         let leftSideNode = MovableSideNode(width: Box.width, height: Box.height, wallThickness: Box.wallThickness, isLeft: true)
         leftSideNode.position = SCNVector3(-horizontalOffset, 0, 0)
         scnScene.rootNode.addChildNode(leftSideNode)
+        sideNodes.append(leftSideNode)
         
         let rightSideNode = MovableSideNode(width: Box.width, height: Box.height, wallThickness: Box.wallThickness, isLeft: false)
         rightSideNode.position = SCNVector3(horizontalOffset, -Box.wallThickness / 2, 0)
         rightSideNode.transform = SCNMatrix4Rotate(rightSideNode.transform, .pi, 0, 1, 0)
         scnScene.rootNode.addChildNode(rightSideNode)
-        
+        sideNodes.append(rightSideNode)
+
         let topSideNode = MovableSideNode(width: Box.width, height: Box.length, wallThickness: Box.wallThickness, isLeft: false)
         topSideNode.position = SCNVector3(Box.wallThickness / 2, verticalOffset, 0)
         topSideNode.transform = SCNMatrix4Rotate(topSideNode.transform, -.pi / 2, 0, 0, 1)
         scnScene.rootNode.addChildNode(topSideNode)
-        
+        sideNodes.append(topSideNode)
+
         let floorSideNode = MovableSideNode(width: Box.width, height: Box.length - Box.wallThickness, wallThickness: Box.wallThickness, isLeft: false)
         floorSideNode.position = SCNVector3(0, -verticalOffset, 0)
         floorSideNode.transform = SCNMatrix4Rotate(floorSideNode.transform, .pi / 2, 0, 0, 1)
         scnScene.rootNode.addChildNode(floorSideNode)
+        sideNodes.append(floorSideNode)
     }
     
+    // MARK: - Gesture actions
+
+    // if tap on side node, toggle between selecting and deselecting it (all others are deselected)
+    // if tap on nothing, deselect all
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {  // Note: system calls tap gesture at start of pan gesture
+        let location = recognizer.location(in: scnView)
+        if let tappedSideNode = getSideNodeAt(location) {
+            // a side node was tapped (toggle its selection)
+            selectedSideNode = selectedSideNode == tappedSideNode ? nil : tappedSideNode
+        } else {
+            // nothing was tapped (deselect all side nodes)
+            selectedSideNode = nil
+        }
+    }
+    
+    // get side node at location provided by tap gesture
+    private func getSideNodeAt(_ location: CGPoint) -> MovableSideNode? {
+        var sideNode: MovableSideNode?
+        let hitResults = scnView.hitTest(location, options: nil)  // nil returns closest hit
+        if let result = hitResults.first(where: { $0.node.parent?.name == "Side" }) {
+            sideNode = result.node.parent as? MovableSideNode
+        }
+        return sideNode
+    }
+
+    // if side selected, move it along pan gesture
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+        if isCameraPanning {
+            recognizer.state = .failed  // force my pan gesture to fail, so camera's pan gesture can take over
+            return
+        }
+    }
+
     // MARK: - Setup functions
     
     private func setupView() {
@@ -82,5 +149,12 @@ class GameViewController: UIViewController {
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(0, 0, 3 * Box.length)
         scnScene.rootNode.addChildNode(cameraNode)
+    }
+
+    // MARK: - UIGestureRecognizerDelegate
+    
+    // allow two simultaneous pan gesture recognizers (mine and the camera's)
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
--- PuzzleBox/MovableSideNode.swift
+++ PuzzleBox/MovableSideNode.swift
@@ -12,11 +12,15 @@ struct Wall {
     static let outsideColors = [#colorLiteral(red: 0.5229098201, green: 0.3702836037, blue: 0.2074212134, alpha: 1), #colorLiteral(red: 0.6524503827, green: 0.4620540142, blue: 0.2582799792, alpha: 1), #colorLiteral(red: 0.7442863584, green: 0.5344663858, blue: 0.3011149168, alpha: 1), #colorLiteral(red: 0.8364808559, green: 0.6006908417, blue: 0.3381323516, alpha: 1)]
     static let middleColors = [#colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1), #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 1), #colorLiteral(red: 0.4756349325, green: 0.4756467342, blue: 0.4756404161, alpha: 1), #colorLiteral(red: 0.5704585314, green: 0.5704723597, blue: 0.5704649091, alpha: 1)]
     static let insideColors = [#colorLiteral(red: 0.5741485357, green: 0.5741624236, blue: 0.574154973, alpha: 1), #colorLiteral(red: 0.6642242074, green: 0.6642400622, blue: 0.6642315388, alpha: 1), #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1), #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)]
+    static let highlightColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
 }
 
 class MovableSideNode: SCNNode {
     
-    static var number = 0
+    static var uniqueNumber = 0
+    
+    private var number = 0
+    private var outside = SCNBox()
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -24,30 +28,39 @@ class MovableSideNode: SCNNode {
     
     init(width: Double, height: Double, wallThickness: Double, isLeft: Bool) {
         super.init()
+        name = "Side"
+        number = MovableSideNode.uniqueNumber
+        MovableSideNode.uniqueNumber += 1
         let outsideHeight = isLeft ? height : height - wallThickness
         let yPosition = isLeft ? Float(wallThickness) / 2 : 0
         
-        let outside = SCNBox(width: wallThickness, height: outsideHeight, length: width - 2 * wallThickness, chamferRadius: 0)
-        outside.firstMaterial?.diffuse.contents = Wall.outsideColors[MovableSideNode.number]
+        outside = SCNBox(width: wallThickness, height: outsideHeight, length: width - 2 * wallThickness, chamferRadius: 0)
+        outside.firstMaterial?.diffuse.contents = Wall.outsideColors[number]
         let outsideNode = SCNNode(geometry: outside)
         outsideNode.position = SCNVector3(x: -Float(wallThickness), y: 0, z: 0)
         outsideNode.physicsBody = SCNPhysicsBody.static()
         addChildNode(outsideNode)
         
         let middle = SCNBox(width: wallThickness, height: height - 3 * wallThickness, length: width - 4 * wallThickness, chamferRadius: 0)
-        middle.firstMaterial?.diffuse.contents = Wall.middleColors[MovableSideNode.number]
+        middle.firstMaterial?.diffuse.contents = Wall.middleColors[number]
         let middleNode = SCNNode(geometry: middle)
         middleNode.position = SCNVector3(x: 0, y: yPosition, z: 0)
         middleNode.physicsBody = SCNPhysicsBody.static()
         addChildNode(middleNode)
         
         let inside = SCNBox(width: wallThickness, height: height - 5 * wallThickness, length: width - 2 * wallThickness, chamferRadius: 0)
-        inside.firstMaterial?.diffuse.contents = Wall.insideColors[MovableSideNode.number]
+        inside.firstMaterial?.diffuse.contents = Wall.insideColors[number]
         let insideNode = SCNNode(geometry: inside)
         insideNode.position = SCNVector3(x: Float(wallThickness), y: yPosition, z: 0)
         insideNode.physicsBody = SCNPhysicsBody.static()
         addChildNode(insideNode)
-        
-        MovableSideNode.number += 1
+    }
+    
+    func highlightColor() {
+        outside.firstMaterial?.diffuse.contents = Wall.highlightColor
+    }
+    
+    func resetColor() {
+        outside.firstMaterial?.diffuse.contents = Wall.outsideColors[number]
     }
 }
